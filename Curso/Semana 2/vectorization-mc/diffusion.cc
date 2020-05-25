fbd227df7c4dbbf4a08fac9ed7485e7777dbdaa2
Marco Antonio Franco <mfranc18@eafit.edu.co>
2020-05-24 20:47:53 -0500

    Updated repo with all the codes

---
+++ Curso/Semana 2/vectorization-mc/diffusion.cc
@@ -0,0 +1,30 @@
+#include <mkl.h>
+#include "distribution.h"
+
+
+//vectorize this function based on instruction on the lab page
+int diffusion(const int n_particles, 
+              const int n_steps, 
+              const float x_threshold,
+              const float alpha, 
+              VSLStreamStatePtr rnStream) {
+  int n_escaped=0;
+  float x[n_particles];
+  x[0:n_particles]=0.0f;
+  float rn[n_particles];
+
+  vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, n_particles, rn, -1.0, 1.0);
+  for (int i = 0; i < n_steps; i++) {
+    for (int j = 0; j < n_particles; j++) {
+      x[j] += dist_func(alpha, rn[j]); 
+    }
+    
+  }
+
+  for (int i = 0; i < n_particles; i++)
+  {
+    if (x[i] > x_threshold) n_escaped++;
+  }
+  
+  return n_escaped;
+}
---
+++ Curso/Semana 4/memory-fft/worker.cc
@@ -0,0 +1,26 @@
+#include <mkl.h>
+#include <hbwmalloc.h>
+
+//implement scratch buffer on HBM and compute FFTs, refer instructions on Lab page
+void runFFTs( const size_t fft_size, const size_t num_fft, MKL_Complex8 *data, DFTI_DESCRIPTOR_HANDLE *fftHandle) {
+  MKL_Complex8 *buff;
+  hbw_posix_memalign((void**) &buff, 4096, sizeof(MKL_Complex8)*fft_size);
+
+  for (size_t j = 0; j < num_fft; j++)
+  {
+    #pragma omp parallel for
+    for(size_t i = 0; i < fft_size; i++) {
+      buff[i].real = data[i + j * fft_size].real;
+      buff[i].imag = data[i + j * fft_size].imag;
+    }
+    DftiComputeForward(*fftHandle, &buff[0]);
+  
+    #pragma omp parallel for
+    for (size_t i = 0; i < fft_size; i++)
+    {
+      data[i + j * fft_size].real = buff[i].real;
+      data[i + j * fft_size].imag = buff[i].imag;
+    }
+  }
+  hbw_free(buff);
+}
